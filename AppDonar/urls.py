e5603d6024e7dc475dbad9aa0fe5422a848abce4
Amaro Ferraris <amaroferraris@gmail.com>
2022-09-22 16:39:53 -0300

    Clase 20 ahora sí: hice todos los pasos hasta la clase 20

--- AppDonar/urls.py
+++ AppDonar/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from AppDonar.views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('ropa/', ropa),
-    path('utensilio/', utensilio),
-    path('mascota/', mascota),
-    path('home/', home),
+    path('', home),
+    path('AppDonar/ropa/', ropa),
+    path('AppDonar/utensilio/', utensilio),
+    path('AppDonar/mascota/', mascota),
+    path('AppDonar/home/', home),
 ]
--- proyectoFinal/urls.py
+++ proyectoFinal/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from proyectoFinal.view import *
-from proyectoFinal.view import inicio
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
-    path('saludo/', saludo),
-    path('template/', templateVista),
-    path('AppDonar/', include("AppDonar.urls")),
+    path('', include("AppDonar.urls")),
 ]
